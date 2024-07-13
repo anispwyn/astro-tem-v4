830661a3d9449e663a31438c89cfb9340acc75ed
anispwyn <anisphia_wynn_palettia@proton.me>
2024-07-14 00:58:53 +0700

    stuff

--- lua/community.lua
+++ lua/community.lua
@@ -5,15 +5,15 @@
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
-  { import = "astrocommunity.pack.lua" },
   -- import/override with your plugins folder
+  { import = "astrocommunity.color.huez-nvim" },
   { import = "astrocommunity.color.transparent-nvim" },
   { import = "astrocommunity.diagnostics.trouble-nvim" },
   { import = "astrocommunity.editing-support.yanky-nvim" },
   { import = "astrocommunity.editing-support.rainbow-delimiters-nvim" },
   { import = "astrocommunity.editing-support.zen-mode-nvim" },
   { import = "astrocommunity.fuzzy-finder.telescope-zoxide" },
-  { import = "astrocommunity.editing-support.rainbow-delimiters-nvim" },
+  { import = "astrocommunity.editing-support.nvim-devdocs" },
   { import = "astrocommunity.git.neogit" },
   { import = "astrocommunity.lsp.ts-error-translator-nvim" },
   { import = "astrocommunity.markdown-and-latex.peek-nvim" },
@@ -35,5 +35,6 @@ return {
   { import = "astrocommunity.pack.python" },
   { import = "astrocommunity.pack.typescript-all-in-one" },
   { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.zig" },
   -- import/override with your plugins folder
 }
